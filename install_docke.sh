1cfd4cb06f3b36437e06c9da9a1a8e5e4689d5bf
R00t <100488601+MrShellcode@users.noreply.github.com>
2024-07-31 04:56:03 +0330

    install_docke.sh

--- install_docke.sh
+++ install_docke.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Step 1: Uninstall conflicting packages
-sudo apt remove docker.io docker-doc docker-compose docker-compose-v2 podman-docker ontainerd runc
+# Step 1: Uninstall any conflicting packages
+sudo apt remove docker.io docker-doc docker-compose docker-compose-v2 podman-docker runc
 
 # Step 2: Set up the Docker repository
-sudo apt install curl
+sudo apt install curl gpg
 sudo printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
 sudo apt update
 
-# Step 2: Install the Docker packages
+# Step 3: Install the Docker packages
 sudo apt install docker-ce
 
-# Step 3: Verify the installation
+# Step 4: Verify the installation
 sudo docker run hello-world
